b0300aba66d29dca180254d03d271db55d359698
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-10-30 15:38:47 +0100

    Create ejecutable del proyecto

--- src/AppMain.java
+++ src/AppMain.java
@@ -2,17 +2,20 @@
 public class AppMain {
 
 	public static void main(String[] args) throws Exception {
+
 		String[] generarClavesPeregrino = { "clavePeregrino" };
 		GenerarClaves.main(generarClavesPeregrino);
-//		String[] generarClavesOficina = { "claveOficina" };
-//		GenerarClaves.main(generarClavesOficina);
-//		String[] generarClavesAlbergue = { "claveAlbergue" };
-//		GenerarClaves.main(generarClavesAlbergue);
-		
-		String[] generarCredencial = {"datos_peregrino.txt", "CPVPack", "clavePeregrino.privada"};
+		String[] generarClavesOficina = { "claveOficina" };
+		GenerarClaves.main(generarClavesOficina);
+		String[] generarClavesAlbergue = { "claveAlbergue" };
+		GenerarClaves.main(generarClavesAlbergue);
+
+// java GenerarCredencial datos_peregrino.txt CPVPack claveOficina.publica clavePeregrino.privada
+		String[] generarCredencial = { "datos_peregrino.txt", "CPVPack", "claveOficina.publica", "clavePeregrino.privada" };
 		GenerarCredencial.main(generarCredencial);
-		
-		String[] desempaquetarCredencial = {"CPVPack", "0", "clavePeregrino.publica"};
+
+// java DesempaquetarCredencial CPVPack 0 claveOficina.privada clavePeregrino.publica
+		String[] desempaquetarCredencial = { "CPVPack", "0", "claveOficina.privada", "clavePeregrino.publica" };
 		DesempaquetarCredencial.main(desempaquetarCredencial);
 
 	}
